17cf6b652cc089ce5fa13f1364995259c69d72af
bhaveshpatel2025p-eng <bhaveshpatel2025.p@gmail.com>
2026-08-14 01:12:39 +0530

    Add files via upload

---
+++ app/build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+android {
+    namespace = "com.nirmaldoor.billing"
+    compileSdk = 35
+    defaultConfig {
+        applicationId = "com.nirmaldoor.billing"
+        minSdk = 24
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+    }
+    buildTypes { release { isMinifyEnabled = false } }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions { jvmTarget = "17" }
+}
